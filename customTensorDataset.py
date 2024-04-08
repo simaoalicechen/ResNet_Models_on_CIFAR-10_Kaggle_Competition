ff8fee33db22f876ac96c41ff53d2ed99e92aaf0
simaoalicechen <alicechen005@gmail.com>
2024-04-08 17:26:41 -0400

    the most basic version

---
+++ customTensorDataset.py
@@ -0,0 +1,71 @@
+#Code from here: https://stackoverflow.com/questions/55588201/pytorch-transforms-on-tensordataset
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, TensorDataset
+
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+
+class CustomTensorDataset(Dataset):
+
+    def __init__(self, tensors, transform=None):
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors)
+        self.tensors = tensors
+        self.transform = transform
+    
+    def __getitem__(self, index):
+        x = self.tensors[0][index]
+
+        if self.transform:
+            x = self.transform(x)
+        
+        y = self.tensors[1][index]
+        return x, y
+
+    def __len__(self):
+        return self.tensors[0].size(0)
+
+def get_transform(split):
+    if split == "train":
+        transform_train = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(degrees=15),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5101, 0.5193, 0.5548), (0.2032, 0.2001, 0.2025)),
+            transforms.RandomErasing()
+        ])
+        return transform_train
+    elif split == "valid":
+        transform_valid = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5101, 0.5193, 0.5548), (0.2032, 0.2001, 0.2025)),
+        ])
+        return transform_valid
+    elif split == "test":
+        transform_test = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5101, 0.5193, 0.5548), (0.2032, 0.2001, 0.2025)),
+        ])
+        return transform_test
+    elif split == "debug":
+        transform_debug = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor(),
+        ])
+        return transform_debug
+    else:
+        print("error, wrong split")
+
+def test_unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        temp_dict = pickle.load(fo, encoding='bytes')
+    return temp_dict
---
+++ main.py
@@ -0,0 +1,214 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import pandas as pd
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torchinfo import summary
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+from models.resnet import ResNet18, ResNet5M
+from customTensorDataset import CustomTensorDataset, get_transform, test_unpickle
+import os
+import argparse
+import pickle
+from models import *
+from utils import progress_bar
+
+import torch
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+def load_cifar_batch(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+# Getting training and validating data: 
+cifar10_dir = 'data/cifar-10-batches-py'
+meta_data_dict = load_cifar_batch(os.path.join(cifar10_dir, 'batches.meta'))
+label_names = meta_data_dict[b'label_names']
+all_images = []
+all_labels = []
+for i in range(1, 6):
+    batch_dict = load_cifar_batch(os.path.join(cifar10_dir, f'data_batch_{i}'))
+    batch_images = batch_dict[b'data'].reshape((10000, 3, 32, 32)).transpose(0, 1, 2, 3)
+    batch_labels = batch_dict[b'labels']
+    all_images.append(batch_images)
+    all_labels.append(batch_labels)
+    train_images_tensor = torch.Tensor(np.concatenate(all_images, axis=0)).to(device)
+    train_labels_tensor = torch.Tensor(np.concatenate(all_labels, axis=0)).to(torch.long).to(device)
+    # train_dataset = TensorDataset(train_images_tensor, train_labels_tensor)
+    # X_train, X_valid, y_train, y_valid = train_test_split(train_images_tensor, train_labels_tensor, test_size=0.2, random_state=42)
+
+# Getting test data here: 
+all_test_images = []
+all_test_labels = []
+batch_dict = test_unpickle('cifar_test_nolabels.pkl')
+batch_images = batch_dict[b'data'].reshape((10000, 3, 32, 32)).transpose(0, 1, 2, 3)
+batch_labels = batch_dict[b'ids']
+all_test_images.append(batch_images)
+all_test_images.append(batch_labels)
+test_images_tensor = torch.Tensor(np.concatenate(all_images, axis=0)).to(device)
+test_labels_tensor = torch.Tensor(np.concatenate(all_labels, axis=0)).to(torch.long).to(device)
+
+# Training dataset
+train_dataset = CustomTensorDataset(tensors=(train_images_tensor, train_labels_tensor), transform=get_transform("train"))
+batch_size = 128
+trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+# Testing dataset
+# test_data, test_labels = load_dataset("test", augment = False)
+test_dataset = CustomTensorDataset(tensors=(test_images_tensor, test_labels_tensor), transform = get_transform("test"))
+batch_size = 400
+testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')      
+net = ResNet5M()
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+import os
+
+checkpoint_dir = './checkpoint/'
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+summary(net, input_size = (400, 3, 32, 32))
+print("Trainable Parameters: "+ str(summary(net, input_size = (400, 3, 32, 32)).trainable_params))
+
+checkpoint_path = './checkpoint/ckpt.pth'
+
+if os.path.exists(checkpoint_path):
+    try:
+        checkpoint = torch.load(checkpoint_path)
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+        print('==> Resuming from checkpoint..')
+    except FileNotFoundError:
+        print(f"Checkpoint file '{checkpoint_path}' not found. Starting from scratch.")
+else:
+    print(f"Checkpoint file '{checkpoint_path}' not found. Starting from scratch.")
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=0.9, weight_decay=5e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+
+def generate_predictions(model, test_loader):
+    # device = get_default_device()  
+    model.eval()  
+    predictions = []
+    with torch.no_grad():
+        for batch in test_loader:
+            images, _ = batch  # Ignore the second element (image_ids)
+            images = images.to(device)
+            outputs = model(images)
+            _, preds = torch.max(outputs, dim=1)
+            predictions.extend(preds.cpu().numpy())  
+    return predictions
+
+def save_predictions_to_csv(predictions, test_ids, csv_filename="predictions.csv"):
+    df = pd.DataFrame({"ID": test_ids, "Labels": predictions})
+    df.to_csv(csv_filename, index=False)
+    print(f"Predictions saved to {csv_filename}")
+
+
+for epoch in range(start_epoch, start_epoch+10):
+    train(epoch)
+    test(epoch)
+    scheduler.step()
+    if epoch == 9:
+        predictions = generate_predictions(net, testloader)
+        save_predictions_to_csv(predictions, list(range(len(predictions))), csv_filename="predictions.csv")
+        print("over")
+
